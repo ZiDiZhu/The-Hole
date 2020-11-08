7001de9b36811efff090464b63f66dc1d148d489
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-11-08 13:59:39 -0500

    Unity Setup

---
+++ Assets/_scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int dayCount = 0;
+    public int playerHealth = 100;
+    public int playerFood = 50;
+
+    public Text healthText;
+    public Text foodText;
+    public Button sleepButton;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = "Health: " + playerHealth;
+        foodText.text = "Food: " + playerFood;
+    }
+
+    public void Sleep()
+    {
+        playerHealth += 20;
+        playerFood -= 20;
+    }
+}
