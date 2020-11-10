e2eb65e54ea097b64493b5b2d4a3ae2c69c6a01e
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-11-10 07:08:26 -0500

    Added Player Script
    Now includes:
    - movement
    - example Action: sleep
    - 3 stats (food,Sanity,Health)

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -5,37 +5,27 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public int floorNum;
+    //public Player player;
 
+    public int floorNum;
     public int monthCount;
     public int dayCount = 0;
-    public int playerHealth = 100;
-    public int playerFood = 50;
 
     public Text monthText;
     public Text dayText;
     public Text timeText;
-    public Text healthText;
-    public Text foodText;
-    public Button sleepButton;
+    public Text floorText;
+    public Text lowerFloorText;
 
     void Start()
     {
-        
+        //player = GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + playerHealth;
-        foodText.text = "Food: " + playerFood;
         dayText.text = "Day " + dayCount;
     }
 
-    public void Sleep()
-    {
-        dayCount += 1;
-        playerHealth += 20;
-        playerFood -= 20;
-    }
 }
---
+++ Assets/_scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb2d;
+    public Vector3 movement;
+
+    public GameManager gm;
+
+    public int foodLevel;
+    public int healthLevel;
+    public int sanityLevel;
+
+    public Button sleepButton;
+    public Button talkButton;
+
+    public Text foodText;
+    public Text healthText;
+    public Text sanityText;
+
+    
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        sleepButton.interactable = false;
+    }
+
+    void Update()
+    {
+        healthText.text = "Health: " + healthLevel + " / 100";
+        foodText.text = "Food: " + foodLevel + " / 100";
+        sanityText.text = "Sanity: " + sanityLevel + " / 100";
+    }
+
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        movement = Vector3.zero;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        rb2d.MovePosition(transform.position + movement * speed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Bed")
+        {
+            Debug.Log("bedtime");
+            sleepButton.interactable = true;
+        }
+    }
+
+    public void Sleep()
+    {
+        healthLevel += 10;
+        foodLevel -= 20;
+
+        gm.dayCount += 1;
+    }
+}
