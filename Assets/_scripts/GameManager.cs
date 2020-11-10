2f0f29cb4784343497ac4d5a44fe9ce3559bc047
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-11-10 05:05:47 -0500

    Added Tilemap and some art assets

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int floorNum;
+
+    public int monthCount;
     public int dayCount = 0;
     public int playerHealth = 100;
     public int playerFood = 50;
 
+    public Text monthText;
+    public Text dayText;
+    public Text timeText;
     public Text healthText;
     public Text foodText;
     public Button sleepButton;
@@ -23,10 +29,12 @@ public class GameManager : MonoBehaviour
     {
         healthText.text = "Health: " + playerHealth;
         foodText.text = "Food: " + playerFood;
+        dayText.text = "Day " + dayCount;
     }
 
     public void Sleep()
     {
+        dayCount += 1;
         playerHealth += 20;
         playerFood -= 20;
     }
