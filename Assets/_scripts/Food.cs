d31448702ce9f6153b5bb36d68b71418f2c754dd
Nebularswamp <40129612+Nebularswamp@users.noreply.github.com>
2020-11-12 03:13:08 -0500

    Added Food Class

---
+++ Assets/_scripts/Food.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour
+{
+    public int foodValue;
+    public int healthValue;
+    public int sanityValue;
+
+    [TextArea(15, 20)]
+    public string description;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -14,6 +14,9 @@ public class GameManager : MonoBehaviour
     public int monthCount;
     public int dayCount = 0;
 
+    public int foodNum;
+    public GameObject[] food;
+
     public Text monthText;
     public Text dayText;
     public Text timeText;
@@ -22,6 +25,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        UpdateEnvironment();
         //player = GetComponent<Player>();
     }
 
@@ -33,15 +37,22 @@ public class GameManager : MonoBehaviour
 
     public void UpdateEnvironment()
     {
+        floorText.text = "" + floorNum;
+
         dayText.text = "Day " + dayCount;
         monthText.text = "Month " + monthCount;
 
-        if (dayPhase == 1) { timeText.text = "A.M."; table.SetActive(false); }
-        else if (dayPhase == 2) { timeText.text = "LUNCH"; table.SetActive(true); }
-        else if (dayPhase == 3) { timeText.text = "P.M."; table.SetActive(false); }
-        else if (dayPhase == 4) { dayCount += 1; dayPhase = 1; }
+        if (dayPhase == 1) { timeText.text = "A.M."; table.SetActive(false);}
+        else if (dayPhase == 2) { timeText.text = "LUNCH"; table.SetActive(true); lowerFloorText.text = ""; GenerateFood(); }
+        else if (dayPhase == 3) { timeText.text = "P.M."; table.SetActive(false); lowerFloorText.text = "" + (floorNum + 1); food[foodNum].SetActive(false); }
+        else if (dayPhase == 4) { dayCount += 1; dayPhase = 1; UpdateEnvironment(); }
 
         if (dayCount == 4) { monthCount += 1; dayCount = 1; } //yes,theres 3days in a month for testing
+    }
+
+    public void GenerateFood()
+    {
+        food[foodNum].SetActive(true);
     }
 
 }
